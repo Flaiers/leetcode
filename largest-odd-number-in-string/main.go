96565a313968bdf4a7e2d1e467f4b6be884f201e
Maksim Bigin <mbigin@ozon.ru>
2023-12-07 23:57:45 +0300

    07.12.2023

---
+++ largest-odd-number-in-string/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func largestOddNumber(num string) string {
+	for i := len(num) - 1; i >= 0; i-- {
+		if int(num[i]-'0')%2 != 0 {
+			return num[:i+1]
+		}
+	}
+
+	return ""
+}
+
+func main() {
+	fmt.Println(largestOddNumber("35427"))
+}
